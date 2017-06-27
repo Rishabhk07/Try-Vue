ca4fd5ebcdc136c380516f3d201c7333cb85ef83
Rishabh Khanna <rishabhkhanna777@gmail.com>
2017-06-27 23:59:50 +0530

    initial commit with vuetify and removed bloat and side anv
    Signed-off-by: Rishabh Khanna <rishabhkhanna777@gmail.com>

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <side-nav-bar></side-nav-bar>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  import SideNavBar from '@/components/SideNav.vue'
+export default {
+    components: {SideNavBar},
+    name: 'app'
+}
+</script>
+<style>
+</style>
---
+++ src/components/SideNav.vue
@@ -0,0 +1,12 @@
+<template>
+  <v-navigation-drawer
+  class="grey lighten-4 pb-0"
+  permanent
+  absolute
+  height="100%"
+  light
+  >
+
+
+  </v-navigation-drawer>
+</template>
