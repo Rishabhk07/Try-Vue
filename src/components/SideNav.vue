1a8874d41c6231848029f64e3a0948fa3eca2903
Rishabh Khanna <rishabhkhanna777@gmail.com>
2017-06-28 00:43:11 +0530

    side bar from store completed
    Signed-off-by: Rishabh Khanna <rishabhkhanna777@gmail.com>

--- src/components/SideNav.vue
+++ src/components/SideNav.vue
@@ -1,13 +1,25 @@
 <template>
 <v-navigation-drawer permanent clipped light>
   <v-list dense class="pt-0">
-    <v-list-item >
+    <v-list-item v-for="navItem in navItems">
       <v-list-tile>
         <v-list-tile-content>
-          <v-list-tile-title>JOBS</v-list-tile-title>
+          <v-list-tile-title >{{ navItem.values }}</v-list-tile-title>
         </v-list-tile-content>
       </v-list-tile>
     </v-list-item>
   </v-list>
 </v-navigation-drawer>
 </template>
+
+<script>
+import store from '@/store'
+export default {
+  name: 'side-navbar',
+  computed: {
+    navItems () {
+      return store.state.SideNavBarItems
+    }
+  }
+}
+</script>
